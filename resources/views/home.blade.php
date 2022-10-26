25cb3627ee421daeca243b1a6f410e9e209c0b3f
Jessé Fernando <62728622+JesseFernando75@users.noreply.github.com>
2022-10-25 21:47:46 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,12 @@
 @extends('layouts.template')
 
     @section('scripts')
-    
     @endsection
 
     @section('title')Template @endsection
 
     @section('estilo')
-    
     @endsection
 
     @section('nav&footer')
-    
     @endsection
