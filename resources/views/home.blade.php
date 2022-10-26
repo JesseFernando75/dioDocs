1264e343bece5e3e67d5126b5d4f6080493d672a
Jessé Fernando <62728622+JesseFernando75@users.noreply.github.com>
2022-10-25 21:50:17 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,35 @@
+@extends('layouts.app')
 @extends('layouts.template')
 
+
+@section('content')
     @section('scripts')
+<div class="container">
     @endsection
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+
+                <div class="card-body">
     @section('title')Template @endsection
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+
+                    {{ __('You are logged in!') }}
     @section('estilo')
+                </div>
     @endsection
+            </div>
 
+        </div>
     @section('nav&footer')
+    </div>
     @endsection
+</div>
+@endsection
